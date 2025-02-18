0416c3a679e476611059dc155bdea110e5365613
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2025-02-18 23:00:00 +0300

    Configure and start up bundled web server. Get rid of autogenerated and currently deactivated stuff.

--- src/api-lite-core.v
+++ src/api-lite-core.v
@@ -14,19 +14,19 @@
 
 module main
 
-//import vweb
-//import databases
-//import os
+import veb
 
-//const port = 8765
+// CustomersApiLiteApp The struct containing data that are shared between
+// different routes.
+struct CustomersApiLiteApp {}
 
-/*struct App {
-    vweb.Context
-}*/
+// RequestContext The struct containing data that are specific to each request.
+struct RequestContext {
+    veb.Context
+}
 
-/*pub fn (app App) before_request() {
-    println('[web] before_request: ${app.req.method} ${app.req.url}')
-}*/
+// server_port The port number used to run the bundled web server.
+const server_port = 8765
 
 // main The microservice entry point.
 //
@@ -34,27 +34,17 @@ module main
 fn main() {
     println('[Customers API Lite]')
 
+    mut app := &CustomersApiLiteApp{}
+
+    // Starting up the bundled web server.
+    veb.run[CustomersApiLiteApp, RequestContext](mut app, server_port)
+
 //  mut db := databases.create_db_connection() or { panic(err) }
-
-/*  sql db {
-        create table User
-        create table Product
-    } or { panic('error on create table: ${err}') }*/
-
+//  sql db {
+//      create table User
+//      create table Product
+//  } or { panic('error on create table: ${err}') }
 //  db.close() or { panic(err) }
-
-/*  mut app := &App{}
-    app.serve_static('/favicon.ico', 'src/assets/favicon.ico')
-    // makes all static files available.
-    app.mount_static_folder_at(os.resource_abs_path('.'), '/')*/
-
-//  vweb.run(app, port)
 }
-
-/*pub fn (mut app App) index() vweb.Result {
-    title := 'vweb app'
-
-    return $vweb.html()
-}*/
 
 // vim:set nu et ts=4 sw=4:
