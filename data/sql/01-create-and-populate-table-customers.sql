f9a7053b26abdacc54b7f8feb017f7b0115e0953
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2025-02-23 01:25:00 +0300

    Bump version number to 0.0.11.

--- data/sql/01-create-and-populate-table-customers.sql
+++ data/sql/01-create-and-populate-table-customers.sql
@@ -1,7 +1,7 @@
 --
 -- data/sql/01-create-and-populate-table-customers.sql
 -- ============================================================================
--- Customers API Lite microservice prototype (V port). Version 0.0.10
+-- Customers API Lite microservice prototype (V port). Version 0.0.11
 -- ============================================================================
 -- A daemon written in V (vlang/veb), designed and intended to be run
 -- as a microservice, implementing a special Customers API prototype
