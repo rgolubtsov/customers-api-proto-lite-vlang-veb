4c8ab282c00545a69ce273075c4a1ae73e8d74ee
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2025-09-19 00:00:00 +0200

    Bump version number to 0.2.7.

--- src/api-lite-helper.v
+++ src/api-lite-helper.v
@@ -1,7 +1,7 @@
 /*
  * src/api-lite-helper.v
  * ============================================================================
- * Customers API Lite microservice prototype (V port). Version 0.2.6
+ * Customers API Lite microservice prototype (V port). Version 0.2.7
  * ============================================================================
  * A daemon written in V (vlang/veb), designed and intended to be run
  * as a microservice, implementing a special Customers API prototype
